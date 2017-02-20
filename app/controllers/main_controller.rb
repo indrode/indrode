32282ff3ae2b2d6daf0ac97841839c95acca2adb
Indro De <indro.de@gmail.com>
2017-02-20 12:41:25 +0100

    Fetch movies and add star-rating helper

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,4 +7,22 @@ class MainController < ApplicationController
 
   def projects
   end
+
+  def playground
+  end
+
+  def movies
+    response = Curl.get("http://localhost:3001/movies") do |http|
+      # http.headers['X-Token'] = Rails.application.secrets.api_token
+      http.headers['Content-Type'] = 'application/json'
+    end
+    @movies = JSON.parse(response.body)['movies']
+
+    @movies_by_location = {
+      'betelgeuse' => @movies.select { |d| d['location'] == 'betelgeuse' }.size,
+      'fenchurch' => @movies.select { |d| d['location'] == 'fenchurch' }.size,
+      'kali' => @movies.select { |d| d['location'] == 'kali' }.size,
+      'vega' => @movies.select { |d| d['location'] == 'vega' }.size
+    }
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def display_stars(score)
+    stars = ''
+    score = (score || 0) / 2.0
+    remaining = 5 - score.floor
+    score.floor.times { stars += icon('star') }
+    remainder = score - score.floor
+    if remainder > 0.24
+      stars += icon('star-half-o')
+      remaining -= 1
+    end
+    remaining.floor.times { stars += icon('star-o') }
+
+    stars
+  end
 end
